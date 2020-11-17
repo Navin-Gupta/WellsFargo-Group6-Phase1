8b3ca0cd1a33b6c6ded69fe621f11c9c1bfbe1ae
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-11-17 16:53:23 +0530

    Add files via upload

--- Phase2/SpringXMLBased/src/com/wf/training/spring/factory/client/MyApp.java
+++ Phase2/SpringXMLBased/src/com/wf/training/spring/factory/client/MyApp.java
@@ -10,15 +10,25 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// Create a container (Bean Factory) based on XML configuration file
+		// All Singleton instances get created here
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// demand-retrieve the beans from factory/container
 		// MessagingService service = context.getBean(MessagingService.class);
-		MessagingService service = context.getBean("smsservice",MessagingService.class);
+		// prototype beans are created here...
+		MessagingService service = context.getBean("emailservice",MessagingService.class);
 		// MessagingService service = context.getBean(EmailService.class);
 		String ack = service.sendMessage("Hello All", "someone");
 		System.out.println(ack);
+		
+		System.out.println("\n------------------\n");
+		MessagingService servicePro = context.getBean("emailservice",MessagingService.class);
+		ack = servicePro.sendMessage("Hello All on another instance", "someone");
+		System.out.println(ack);
+		
+		// stop/terminate the container
+		context.close();
 		
 	}
 
