9696193bfda76412cdc71ffd4f275c37d709a413
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-12-10 20:12:44 +0530

    Add files via upload

---
+++ Phase3/bootapp-restful/src/main/java/com/wf/training/bootapprestful/BootappRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.wf.training.bootapprestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootappRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootappRestfulApplication.class, args);
+	}
+
+}
